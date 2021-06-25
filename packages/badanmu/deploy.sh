f1510390d4a79765f668c8d59049631c8a2288fc
i18u <iahu1988@gmail.com>
2021-06-25 16:29:19 +0800

    Fix depoly env

--- packages/badanmu/deploy.sh
+++ packages/badanmu/deploy.sh
@@ -1,3 +1,5 @@
+export PATH=$PATH:/cygdrive/c/Users/bin/AppData/Roaming/npm:$PWD/node_modules/.bin
+
 for (( i = 8000; i < 8003; i++ )); do
   pm2 startOrRestart ecosystem.config.js --only "badanmu-$i";
   sleep 2
